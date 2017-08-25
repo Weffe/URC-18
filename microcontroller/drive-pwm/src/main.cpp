1a3658a2e5ef08083d78041356fd83ac1d000f43
mincrmatt12 <matthewmirvish@hotmail.com>
2017-08-24 22:45:01 -0400

    done?

--- microcontroller/drive-pwm/src/main.cpp
+++ microcontroller/drive-pwm/src/main.cpp
@@ -4,11 +4,15 @@
 
 #include <Servo.h>
 #include <Arduino.h>
+#include <Wire.h>
 
 Servo** servos;
 uint8_t servoCount = 0;
+uint8_t lastName = 0;
 
-uint8_t addServo(uint16_t pin) {
+const int ADDRESS = 0x40;
+
+uint8_t addServo(uint8_t pin) {
     if (servoCount != 0) {
         servos = static_cast<Servo **>(realloc(servos, sizeof(Servo *) * servoCount + 1));
     }
@@ -21,10 +25,42 @@ uint8_t addServo(uint16_t pin) {
     return servoCount - 1;
 }
 
-void setup() {
+void setServo(uint8_t name, uint16_t micros) {
+    if (name < servoCount) {
+        servos[name]->writeMicroseconds(micros);
+    }
+}
 
+void onRecieve(int bytes) {
+    int func = Wire.read();
+    if (bytes == 2 && func == 0x01) {
+        lastName = addServo(static_cast<uint8_t>(Wire.read()));
+    }
+    else if (bytes == 4 && func == 0x02) {
+        uint8_t servo = static_cast<uint8_t>(Wire.read());
+        uint8_t buf[2];
+        Wire.readBytes(buf, 2);
+        setServo(servo, reinterpret_cast<uint16_t>(buf));
+    }
+    else {
+        bytes--;
+        while (bytes > 0) {
+            Wire.read(); // scoop it up
+            bytes--;
+        }
+    }
+}
+
+void onRequest() {
+    Wire.write(lastName);
+}
+
+void setup() {
+    Wire.begin(ADDRESS);
+    Wire.onRequest(onRequest);
+    Wire.onReceive(onRecieve);
 }
 
 void loop() {
-
+    delay(50); // yaaaay
 }
